f5abdbf001c771be591a787407dcfe5848640096
MAHossain1 <mahpro110@gmail.com>
2023-07-15 22:41:33 +0600

    post book api done

--- index.js
+++ index.js
@@ -28,6 +28,26 @@ const run = async () => {
       const books = await bookCollection.find({}).toArray();
       res.send({ status: true, data: books });
     });
+
+    app.post("/books", async (req, res) => {
+      try {
+        const { title, author, genre, publicationDate } = req.body;
+
+        // Add the new book to the database
+        const newBook = {
+          title,
+          author,
+          genre,
+          publicationDate,
+        };
+        await bookCollection.insertOne(newBook);
+
+        res.send({ status: true, message: "Book added successfully" });
+      } catch (error) {
+        console.error("Failed to add book", error);
+        res.status(500).send({ status: false, message: "Failed to add book" });
+      }
+    });
   } finally {
   }
 };
